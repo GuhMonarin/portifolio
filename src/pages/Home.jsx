335d5cb1ba47dfcfb6961e86a3a604609064ebe7
Gustavo Monarin dos Santos <gustavomonarin11@gmail.com>
2025-08-28 17:40:20 -0300

    adicionado o `Background.jsx`

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,9 +1,11 @@
-import { Themetogle } from "../components/Themetogle";
+import { Background } from "../components/Background";
+import { Themetogle } from "../components/Themetoggle";
 
 export const Home = () => {
   return (
     <div className="min-h-screen bg-background text-foreground overflow-x-hidden">
       <Themetogle />
+      <Background />
     </div>
   );
 };
