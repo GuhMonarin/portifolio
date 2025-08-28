5a05a85abb2e8621e41dabd9abc90aed194bb444
Gustavo Monarin dos Santos <gustavomonarin11@gmail.com>
2025-08-28 11:22:35 -0300

    configuracao do modo claro e modo escuro add o components `Themetogle.jsx`

---
+++ src/pages/Home.jsx
@@ -0,0 +1,9 @@
+import { Themetogle } from "../components/Themetogle";
+
+export const Home = () => {
+  return (
+    <div className="min-h-screen bg-background text-foreground overflow-x-hidden">
+      <Themetogle />
+    </div>
+  );
+};
